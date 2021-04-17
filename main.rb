183079926a638e38328fbb24d249c5383b29a902
yosnak13 <yosnak13@gmail.com>
2021-04-18 07:40:39 +0900

    define class Brave

---
+++ main.rb
@@ -0,0 +1,19 @@
+class Brave
+
+  def name=(name)
+    @name = name
+  end
+
+  def hp=(hp)
+    @hp = hp
+  end
+
+  def offense=(offense)
+    @offense = offense
+  end
+
+  def defence(defence)
+    @defence = defence
+  end
+
+end
