37edb066afcf189d727319dd3e73f94afb37dc9c
yosnak13 <yosnak13@gmail.com>
2021-04-18 07:53:09 +0900

    ゲッターと出力コード

--- main.rb
+++ main.rb
@@ -1,19 +1,36 @@
 class Brave
-
+  #nameのセッター（インスタンス変数の値をクラス内で更新するためのメソッド）
   def name=(name)
     @name = name
+  end
+
+  #nameのゲッター（インスタンス変数の値を取得してクラス外から参照可能）
+  def name
+    @name
   end
 
   def hp=(hp)
     @hp = hp
   end
 
+  def hp
+    @hp
+  end
+
   def offense=(offense)
     @offense = offense
   end
 
+  def offense
+    @offense
+  end
+
   def defense=(defense)
     @defense = defense
+  end
+
+  def defense
+    @defense
   end
 
 end
@@ -24,3 +41,8 @@ brave.name = "テリー"
 brave.hp = 500
 brave.offense = 150
 brave.defense = 100
+
+puts brave.name
+puts brave.hp
+puts brave.offense
+puts brave.defense
