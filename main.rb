30a03b020d821c1c972839a244a0e197412d0c4b
yosnak13 <yosnak13@gmail.com>
2021-04-18 15:10:42 +0900

    intializeメソッドでインスタンス変数の初期値を設定

--- main.rb
+++ main.rb
@@ -1,32 +1,29 @@
 class Brave
-  #nameのセッター（インスタンス変数の値をクラス内で更新するためのメソッド）
-  def name=(name)
-    @name = name
+
+  def initialize
+    @name = "テリー"
+    @hp = 500
+    @offense = 150
+    @defense = 100
   end
+
+  #nameのセッター（インスタンス変数の値をクラス内で更新するためのメソッド）
+  #initializeで不要に
+  # def name=(name)
+  #   @name = name
+  # end
 
   #nameのゲッター（インスタンス変数の値を取得してクラス外から参照可能）
   def name
     @name
   end
 
-  def hp=(hp)
-    @hp = hp
-  end
-
   def hp
     @hp
   end
 
-  def offense=(offense)
-    @offense = offense
-  end
-
   def offense
     @offense
-  end
-
-  def defense=(defense)
-    @defense = defense
   end
 
   def defense
@@ -36,11 +33,6 @@ class Brave
 end
 
 brave = Brave.new
-
-brave.name = "テリー"
-brave.hp = 500
-brave.offense = 150
-brave.defense = 100
 
 puts <<~TEXT
 NAME:#{brave.name}
