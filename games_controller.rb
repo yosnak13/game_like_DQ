7e123f09bef8036cee264a6e799bd72014720609
yosnak13 <yosnak13@gmail.com>
2021-04-25 18:14:52 +0900

    キャラクターのインスタンスをインスタンス変数に格納。引数を最低限にする

--- games_controller.rb
+++ games_controller.rb
@@ -3,22 +3,21 @@ class GamesController
   GOLD_CONSTANT = 3
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_characters(params)
 
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @brave.attack(@monster)
+      break if battle_end?#引数不要に
+      @monster.attack(@brave)
+      break if battle_end?#引数不要に
     end
 
-    if battle_result(brave)
-      result = calculate_of_exp_and_gold(monster)
-      puts "#{brave.name}は戦いに勝った"
+    if battle_result
+      result = calculate_of_exp_and_gold#引数不要に
+      puts "#{@brave.name}は戦いに勝った"
       puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
     else
-      puts "#{brave.name}は戦いに負けた"
+      puts "#{@brave.name}は戦いに負けた"
       puts "目の前が真っ暗になった"
     end
   end
@@ -26,15 +25,21 @@ class GamesController
 
   private
 
-  def battle_end?(character)
-    character.hp <= 0
+  #キャラクターのインスタンスをインスタンス変数に格納
+  def build_characters(**params)
+    @brave = params[:brave]
+    @monster = params[:monster]
   end
 
-  def battle_result(brave)
-    brave.hp <= 0
+  def battle_end?#引数不要に
+    character.hp <= 0 || @mosnter.hp <= 0
   end
 
-  def calculate_of_exp_and_gold(mosnter)
+  def battle_result#引数不要に
+    @brave.hp > 0
+  end
+
+  def calculate_of_exp_and_gold#引数不要に
     exp = (monster.offense + monster.defense) * EXP_CONSTANT
     gold = (monster.offense + monster.defense) * GOLD_CONSTANT
     result = {exp:exp, gold:gold}
