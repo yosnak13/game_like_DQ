fa2a6a3e9b9c0a43ddfca6a735fc9f9137d49c35
yosnak13 <yosnak13@gmail.com>
2021-04-26 07:45:19 +0900

    ループ処理、勝者判定、経験値処理などをgames_controllerに切り出し

--- main.rb
+++ main.rb
@@ -1,29 +1,10 @@
 require './brave'
 require './monster'
+require './games_controller'
 
+games_controller = GamesController.new
 
-brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+terry = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+slime = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-EXP_CONSTANT = 2
-GOLD_CONSTANT = 3
-
-loop do
-  brave.attack(monster)
-  break if monster.hp <= 0
-
-  monster.attack(brave)
-  break if brave.hp <= 0
-end
-
-battle_result = brave.hp > 0
-
-if battle_result
-  exp = (monster.offense + monster.defense) * EXP_CONSTANT
-  gold = (monster.offense + monster.defense) * GOLD_CONSTANT
-  puts "#{brave.name}は戦いに勝った"
-  puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
-else
-  puts "#{brave.name}は戦いに負けた"
-  puts "目の前が真っ暗になった"
-end
+games_controller.battle(brave: terry, monster: slime)
