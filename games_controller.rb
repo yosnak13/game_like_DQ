1954c519c04c6ab8b2e24c8e0f5b6e969a263795
yosnak13 <yosnak13@gmail.com>
2021-04-25 18:22:29 +0900

    勇者の勝敗をbattle_jadgementメソッドに切り出し

--- games_controller.rb
+++ games_controller.rb
@@ -12,14 +12,16 @@ class GamesController
       break if battle_end?#引数不要に
     end
 
-    if battle_result
-      result = calculate_of_exp_and_gold#引数不要に
-      puts "#{@brave.name}は戦いに勝った"
-      puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
-    else
-      puts "#{@brave.name}は戦いに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    # if battle_result
+    #   result = calculate_of_exp_and_gold#引数不要に
+    #   puts "#{@brave.name}は戦いに勝った"
+    #   puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+    # else
+    #   puts "#{@brave.name}は戦いに負けた"
+    #   puts "目の前が真っ暗になった"
+    # end
+
+    battle_judgment
   end
 
 
@@ -35,8 +37,21 @@ class GamesController
     character.hp <= 0 || @mosnter.hp <= 0
   end
 
-  def battle_result#引数不要に
+  def brave_win? #引数不要に
+    # boolean型のデータが返るメソッドには末尾に?をつける
     @brave.hp > 0
+  end
+
+  #勇者の勝敗によりメッセージを変更するメソッド
+  def battle_judgment
+    if brave_win?
+      result = calculate_of_exp_and_gold
+      puts "#{@brave.name}は戦いに勝った"
+      puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+    else
+      puts "#{@brave.name}は戦いに負けた"
+      puts "目の前が真っ暗になった"
+    end
   end
 
   def calculate_of_exp_and_gold#引数不要に
