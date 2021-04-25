9224e37b9672c96afa0883899c3e793ddb3477e5
yosnak13 <yosnak13@gmail.com>
2021-04-25 16:45:02 +0900

    経験値、賞金を計算する定数を記載

--- main.rb
+++ main.rb
@@ -5,6 +5,9 @@ require './monster'
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
+EXP_CONSTANT = 2
+GOLD_CONSTANT = 3
+
 loop do
   brave.attack(monster)
   break if monster.hp <= 0
@@ -16,8 +19,8 @@ end
 battle_result = brave.hp > 0
 
 if battle_result
-  exp = (monster.offense + monster.defense) * 2
-  gold = (monster.offense + monster.defense) * 3
+  exp = (monster.offense + monster.defense) * EXP_CONSTANT
+  gold = (monster.offense + monster.defense) * GOLD_CONSTANT
   puts "#{brave.name}は戦いに勝った"
   puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
 else
